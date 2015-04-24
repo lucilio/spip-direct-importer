8303ed663c589fe27c1b002f4c6b59ae80df6083
L! <lucilio@lucilio.net>
2015-04-24 03:53:22 -0300

    perdas do salvamento indevido estão reparadas. continuando...

--- index.php
+++ index.php
@@ -29,40 +29,35 @@ class SpipMediaImporter
 
 	function __get( $property ){
 		if( $property == 'options' ){
-			return get_option( 'spip_import', array() );
+			$options = get_option( 'spip_import', array() );
+			return $options;
 		}
-		elseif( $property == 'connection' ){
-			$link = NULL;
+		elseif( $property == 'imported_setup' ){
 			$options = $this->options;
-			if( function_exists('mysql_connect') ){
-				if( isset( $options['connection'] ) ){
-					$link = mysql_connect(
-						$options['connection']['host'],
-						$options['connection']['username'],
-						$options['connection']['password']
-						)
-					;
-					if( $link and mysql_select_db( $this->options['connection']['database'], $link ) ){
-						$options['imported_setup'] = array();
-						$results = mysql_query( "SELECT * FROM spip_meta;" );
-						while( $row = mysql_fetch_assoc( $results ) ){
-							$options['imported_setup'][ $row['nom'] ] = $row['valeur'];
-						}
-						update_option( 'spip_import', $options );
+			if( array_key_exists( 'imported_setup', $options ) ){
+				return $options['imported_setup'];
+			}
+			elseif( array_key_exists( 'connection', $options ) ){
+				$imported_setup = array();
+				$results = mysql_query( "SELECT * FROM spip_meta;" );
+				if( $results ){
+					while( $row = mysql_fetch_assoc( $results ) ){
+						$imported_setup[ $row['nom'] ] = $row['valeur'];
 					}
-					else{
-						$link = FALSE;
-					}
+					return $imported_setup;
 				}
 			}
-			else{
-				?>
-				<div id="spip-media-import-disconnected" class="error settings-error">
-					<p><strong><?php echo __('MySQL library is missing') ?></strong></p>
-				</div>
-				<?php
+			return FALSE;
+		}
+		elseif( $property == 'connection' ){
+			$options = $this->options;
+			if( function_exists('mysql_connect') ){
+				$link = mysql_connect( $options['connection']['host'], $options['connection']['username'], $options['connection']['password'] );
+				if( $link and  mysql_select_db( $options['connection']['database'], $link ) ){
+					return $link;
+				}
 			}
-			return $link;
+			return FALSE;
 		}
 		else{
 			/*DEBUG*/die('erro grave: ' . compact('property'));
@@ -187,23 +182,29 @@ class SpipMediaImporter
 	}
 
 	function spip_import_users(){
+		$options = $this->options;
 		$results = $this->query( "SELECT BINARY login AS user_login, BINARY nom AS display_name, BINARY email AS user_email FROM spip_auteurs;" );
 		?>
 		<label>
-			<input type="checkbox" id="spip_import_user_import__all" name="spip_import[do_importing][user][import__all]">
+			<input type="checkbox" id="spip_import_user_import__all" name="spip_import[do_importing][user][import__all]"<?php echo ( ( is_array( $options['do_importing']['users'] ) and in_array( 'import__all', $options['do_importing']['users'] ) )  ? ' checked' : '' );?>>
 			<strong><?php echo __('Import all users'); ?></strong>
 		</label>
 		<dl>
 		<?php
 		while( $results and $row = mysql_fetch_assoc( $results ) ){
 			extract( $row );
-			if( $this->get_import_status( 'user', $user_login ) ){
-				die( 'X__x' );
+			if( is_array( $this->options['do_importing']['user'] ) and in_array( $user_login , $this->options['do_importing']['user'] ) ){
+				$cheked = ' checked';
+				echo "should be imported...";
 			}
+			else{
+				$checked = '';
+			}
+			$disabled = username_exists( $user_login ) ? '' : ' disabled';
 			?>
 			<dt>
 				<label for="spip_import_<?php echo $user_login; ?>">
-					<input id="spip_import_user_<?php echo $user_login; ?>" type="checkbox" name="spip_import[do_importing][user][<?php echo $user_login; ?>]" title="Import <?php echo $user_login; ?>" >
+					<input id="spip_import_user_<?php echo $user_login; ?>" type="checkbox" name="spip_import[do_importing][user][<?php echo $user_login; ?>]" title="Import <?php echo $user_login; ?>"<?php echo "{$checked}{$disabled}"; ?>>
 					<?php echo ( empty( $display_name ) ? $user_login : $display_name ) ?>
 				</label>
 			</dt>
@@ -249,7 +250,7 @@ class SpipMediaImporter
 	}
 
 	function spip_import_posts(){
-		$results = $this->query( "SELECT id_article AS spip_id, BINARY titre AS post_title, BINARY texte AS post_content, statut AS post_status, date AS post_date FROM spip_articles;" );
+		$results = $this->query( "SELECT id_article AS spip_id, BINARY titre AS post_title, BINARY texte AS post_content, statut AS post_status, date AS post_date, id_rubrique AS category FROM spip_articles;" );
 		?>
 		<ul>
 			<?php
@@ -274,7 +275,7 @@ class SpipMediaImporter
 						<em><strong><?php echo $post_author; ?></strong></em>
 						[<?php echo $spip_id ?>]
 					</p>
-					<p><em><?php echo implode( '</em> <strong>-&gt;</strong> <em>' , $this->get_category_chain() ) ?></em></p>
+					<p><em><?php echo implode( '</em> <strong>-&gt;</strong> <em>' , $this->get_category_chain( $category ) ) ?></em></p>
 					<hr>
 				</li>
 				<?php
@@ -293,28 +294,28 @@ class SpipMediaImporter
 		}
 		</style>
 		<script type="text/javascript">
-		jQuery(document).on('change','input[name^=spip_import]',function(e){
-			inputNameKeys = e.target.match(/spip_import\[(.*)\]/)[1].split('][');
-			while( inputNameKeys.length && currentKey = inputNameKeys.pop() ){
-				if( currentKey == 'import__all' ){
-					jQuery('[name^=spip_import\\[' + inputNameKeys.join('\\]\\[') + '\\]').prop('checked', e.target.checked );
+		jQuery(document).ready( function(){
+			jQuery(document).on('change','input[name^=spip_import]',function(e){
+				inputNameKeys = e.target.name.match(/spip_import\[do_importing\]\[(.*)\]/)[1].split('][');
+				while( inputNameKeys.length && ( currentKey = inputNameKeys.pop() ) ){
+					var fieldSet = jQuery('[name^=spip_import\\[do_importing\\]\\[' + inputNameKeys.join('\\]\\[') + '\\]]' ).not('[name^=spip_import\\[' + inputNameKeys.join('\\]\\[') + '\\]\\[' + currentKey + '\\]]' ).not('[name*=\\[import__all\\]]');
+					if( currentKey == 'import__all' ){
+						fieldSet.prop('checked', e.target.checked );
+					}
+					else if(fieldSet.length){
+						importAll = jQuery('[name=spip_import\\[do_importing\\]\\[' + inputNameKeys.join('\\]\\[') + '\\]\\[import__all\\]]')
+						importAll.prop( 'checked', fieldSet.map(function(em){return fieldSet[em].checked}).toArray().reduce(function(p,c){return p && c}) );
+					}
 				}
-				else{
-					temp = inputNameKeys.pop();
-					fieldSet = jQuery('name^=spip_import\\[' + inputNameKeys.join('\\]\\[') + '\\]' );
-					fieldSet.prop( 'checked', fieldSet.map( function( element ){ return element.checked } ).toArray().reduce( function( previous, current ){ return previous && current } ) );
-					inputNameKeys.push( temp );
-				}
-			}
+			});
 		});
-		
 		</script>
 		<?php
 		submit_button();
 	}
 
 	function spip_import_options_check( $input ){
-		if( !isset( $input['connection']['reset'] ) ){
+		if( !isset( $input['connection']['reset'] ) or ( !$input['connection']['reset'] ) ){
 			if( isset( $input['connection']['host'] ) and empty( $input['connection']['host'] ) ){
 				add_settings_error( 'spip_import', 'connection-host', __('Please set the server host'), 'error' );
 			}
@@ -324,8 +325,15 @@ class SpipMediaImporter
 			if( isset( $input['connection']['password'] ) and empty( $input['connection']['password'] ) ){
 				add_settings_error( 'spip_import', 'connection-password', __('Password can not be empty'), 'error' );
 			}
-			if( !$this->connection ){
-				add_settings_error( 'spip_import', 'connection-faillure', mysql_error(), 'error' );
+			if( isset( $input['connection']['database'] ) and empty( $input['connection']['database'] ) ){
+				add_settings_error( 'spip_import', 'connection-database', __('Please provide the name of the database to connect'), 'error' );
+			}
+			if( isset( $input['do_importing'] ) ){
+				foreach( $input['do_importing'] as $object => $list ){
+					foreach( $list as $item => $true ){
+						$input['do_importing'][ $object ][] = $item;
+					}
+				}
 			}
 			$input['connection']['reset'] = FALSE;
 		}
@@ -370,6 +378,7 @@ class SpipMediaImporter
 			<input type="checkbox" id="spip_import_connection_reset" name="spip_import[connection][reset]"<?php echo ( $reset ? " checked" : "" ); ?>>
 			<code style="color: red;"><?php echo"{$this->options[connection][username]}@{$this->options[connection][host]}"; ?></code>			
 		</label>
+		<hr>
 		<?php
 	}
 
@@ -403,12 +412,14 @@ class SpipMediaImporter
 	}
 
 	function query( $SQL ){
-		$connection = $this->connection;
-		$results = mysql_query( $SQL, $connection );
-		if( !$results ){
-			echo mysql_error();
+		if( $connection = $this->connection ){
+			$results = mysql_query( $SQL, $connection );
+			if( !$results ){
+				echo mysql_error();
+			}
+			return $results;
 		}
-		return $results;
+		return FALSE;
 	}
 
 	function parse_content( $content ){
@@ -476,7 +487,7 @@ class SpipMediaImporter
 	function parse_media( $media_id ){
 		$results = $this->query("SELECT fichier AS file, largeur AS width, hauteur AS height, titre AS title, date FROM spip_documents WHERE id_document = $media_id");
 		$media = mysql_fetch_assoc( $results );
-		$media['file'] =   $this->options['imported_setup']['adresse_site'] . '/' . $this->options['imported_setup']['dir_img'] . '/' .  $media['file'];
+		$media['file'] =   $this->imported_setup['adresse_site'] . '/' . $this->imported_setup['dir_img'] . '/' .  $media['file'];
 		return $media;
 	}
 
@@ -519,16 +530,20 @@ class SpipMediaImporter
 		return $category_chain;
 	}
 
-	function get_import_status( $object = NULL, $id = NULL ){
+	function test_connection(){
 		$options = $this->options;
-		if( array_key_exists( 'spip_import' , $_POST ) ){
-			die( var_export( $_POST['spip_import'] ) );
+		if( $options['imported_setup'] = $this->imported_setup ){
+			return true;
 		}
+		add_settings_error( 'spip_import', 'connection-faillure',  __('Invalid connection settings') . ': ' . mysql_error(), 'error' );
+		return false;
+
 	}
 
 	function reset_connection(){
 		$options = $this->options;
 		$options['connection'] = NULL;
+		$options['imported_setup'] = NULL;
 		update_option( 'spip_import', $options );
 	}
 
